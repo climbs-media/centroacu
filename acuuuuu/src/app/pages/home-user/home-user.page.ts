450a1a552862908271da1f5a663cd9161f79783d
desarrollos.chadwick <desarrollos.chadwick@gmail.com>
2019-05-12 11:53:42 +0200

    Solucion de Grafica Paliativa para entregar algo medio decente al cliente

--- acuuuuu/src/app/pages/home-user/home-user.page.ts
+++ acuuuuu/src/app/pages/home-user/home-user.page.ts
@@ -64,6 +64,21 @@ public chartHovered(e: any): void {
   console.log(e);
 }
 
+public barChartOptions = {
+  scaleShowVerticalLines: false,
+  responsive: true
+};
+
+public barChartLabels = ['40', '50 ', '60', '70', '80', '100', '120'];
+public barChartType = 'bar';
+public barChartLegend = true;
+
+public barChartData = [
+  {data: [1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2], label: 'Altura'},
+  {data: [45, 55, 65, 75, 85, 95, 100, 110, 120], label: 'Peso Kg'},
+  {data: [1.85,2.85, 3.85, 5.25, 25.5, 30, 31, 50], label: 'IMC'}
+];
+
 
 
 }
